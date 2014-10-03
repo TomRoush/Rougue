e0de2fe58314b2c2eb3e7ccad31f8fb514997918
Pavle Simonovic <elvapsimonovic@gmail.com>
2014-10-03 12:41:33 -0500

    Added ability to pause.

--- Assets/Scripts/Entities/Player/Player.cs
+++ Assets/Scripts/Entities/Player/Player.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class Player : MonoBehaviour 
 {
 	public float speed;
+	public bool paused = false;
 	private Vector3 moveDirection;
 	public float turnSpeed;
 	void Start () {
@@ -30,30 +31,55 @@ public class Player : MonoBehaviour
 			//transform.position = Vector3.Lerp( currentPosition, target, Time.deltaTime );
 		}
 		//*/
-		if (Input.GetKey (KeyCode.W)) 
+		if (Input.GetKey (KeyCode.W) || Input.GetKey (KeyCode.UpArrow)) 
 		{
 			rigidbody2D.transform.position += Vector3.up * speed * Time.deltaTime;
 		}
-		if (Input.GetKey (KeyCode.A)) 
+		if (Input.GetKey (KeyCode.A) || Input.GetKey (KeyCode.LeftArrow)) 
 		{
 			rigidbody2D.transform.position += Vector3.left * speed * Time.deltaTime;
 		}
-		if (Input.GetKey (KeyCode.S)) 
+		if (Input.GetKey (KeyCode.S) || Input.GetKey (KeyCode.DownArrow)) 
 		{
 			rigidbody2D.transform.position += Vector3.down * speed * Time.deltaTime;
 		}
-		if (Input.GetKey (KeyCode.D)) 
+		if (Input.GetKey (KeyCode.D) || Input.GetKey (KeyCode.RightArrow)) 
 		{			
 			rigidbody2D.transform.position += Vector3.right * speed * Time.deltaTime;
 		}
+		if (Input.GetKeyDown (KeyCode.Escape) && !paused) 
+		{
+			paused = true;
+			UpdateGameState();
+		} else if (Input.GetKeyDown (KeyCode.Escape) && paused) 
+		{
+			paused = false;
+			UpdateGameState();
+		}
 	}
 
+	void OnGUI()
+	{
+		if(paused)
+		{
+			GUI.Label(new Rect(50, 50, 75, 75), "PAUSED");
+		}
+	}
 	void OnTriggerEnter2D( Collider2D other )
 	{
 		if(other.CompareTag("goal")) 
 		{
 			Application.LoadLevel ("TileMapTester");
 		}
+	}
+
+	public void UpdateGameState()
+	{
+		Time.timeScale = paused ? 0 : 1;
+//		if (paused) 
+//		{
+//			OnGUI ();
+//		}
 	}
 
 	public void Respawn(Vector3 spawnPt)
