90ade602a3ccec8fee169daeaa0db47f27f803c6
Wyatt Duiker <duiker2@illinois.edu>
2014-10-15 10:30:08 -0500

    Fixed down & left animation glitch

--- Assets/Scripts/Entities/Player/Player.cs
+++ Assets/Scripts/Entities/Player/Player.cs
@@ -62,7 +62,8 @@ public class Player : Entities
 		}
 		if (Input.GetKey (KeyCode.S) || Input.GetKey (KeyCode.DownArrow)) 
 		{
-			anim.SetBool("d",true);
+			if (!Input.GetKey (KeyCode.A) && !Input.GetKey (KeyCode.LeftArrow)) 
+				anim.SetBool("d",true);
 			rigidbody2D.transform.position += Vector3.down * speed * Time.deltaTime;
 		}
 		if (Input.GetKey (KeyCode.D) || Input.GetKey (KeyCode.RightArrow)) 
