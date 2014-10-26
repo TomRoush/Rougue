9862c17ddc52289e79b8d92b67a0c437927e5e5c
Edward Kim <jimmy04500@gmail.com>
2014-10-26 13:17:26 -0500

    added comments
    added comments to destroybullet.cs and weapon.cs
    try to pull just the comments

--- Assets/Scripts/Combat/DestroyBullet.cs
+++ Assets/Scripts/Combat/DestroyBullet.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+// Attached to projectile prefabs to handle collision behavior
 public class DestroyBullet : MonoBehaviour {
 
 	// Use this for initialization
@@ -9,13 +10,19 @@ public class DestroyBullet : MonoBehaviour {
 	}
 
 	void OnCollisionEnter2D(Collision2D coll) {
+		// If collided with enemy
 		if (coll.gameObject.tag == "Enemy") {
 			Debug.Log ("collisionenemy");
+			// Destroy bullet
 			Destroy (gameObject);
-
+			// Decrease health of enemy
 			coll.gameObject.GetComponent<AttackingMob>().takeHealth(30);
+		
+		// If collided with wall (doesn't work right now, not sure why)
 		} else if (coll.gameObject.tag == "Wall") {
 			Debug.Log ("collisionwall");
+		
+		// Else, destroy bullet after 0.5 secs
 		} else {
 			Destroy (gameObject, 0.5f);
 		}
