9024cfa4d6741cc61a5e1a684f150b8db4877863
John5025 <Moon50255025@yahoo.com>
2014-10-25 22:06:20 -0500

    Ranged autoattack, not longer attack on collision

---
+++ Assets/Scripts/Combat/Buff Debuff.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BuffDebuff : TimedEffect {
+	
+	//public BaseStat stat;
+	//public int buffValue;
+	
+	//protected override void ApplyEffect (string stat)
+	//{
+		//target.BuffStat(stat, buffValue);
+		//target.GetComponent<Status>().; 
+	//}
+	
+	//protected override void EndEffect ()
+	//{
+		//target.BuffStat(stat, -buffValue);
+	//	base.EndEffect ();
+	//}
+}
