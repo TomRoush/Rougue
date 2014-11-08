2de81e56eaed5abe0243931a695b26e7fd51a5ba
Edward Kim <jimmy04500@gmail.com>
2014-11-08 15:34:46 -0600

    ai new

---
+++ Assets/Scripts/Entities/Enemies/AI/Location.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Location {
+
+	public int x;
+	public int y;
+
+	public Location(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public bool equals(Location l) {
+		return l.x == x && l.y == y;
+	}
+}
--- Assets/Scripts/Entities/Enemies/AttackingMob.cs
+++ Assets/Scripts/Entities/Enemies/AttackingMob.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class AttackingMob : Entities {
 
@@ -11,6 +12,10 @@ public class AttackingMob : Entities {
 
 	private bool canAttack;
 
+	private MovementAI ai;
+	private List<Node> path;
+	private Vector3 target;
+
 	void Start () {
 
         InitializeEntity();
@@ -21,13 +26,28 @@ public class AttackingMob : Entities {
 			attackingg = GameObject.FindGameObjectWithTag("Player");
 			attacking = attackingg.GetComponent<Entities>();
 		}
+
+		MakeMap mapgen = GameObject.FindGameObjectWithTag("MapGen").GetComponent<MakeMap>();
+		ai = new MovementAI (mapgen.currentFloor ());
+		path = ai.getPath (gameObject.transform.position, attackingg.transform.position);
 	}
 	
 
 	void FixedUpdate () {
-        
-        this.setDirection(attackingg.transform.position - this.transform.position);
-        Move();
+		if (ai.fpscounter > 40) {
+			ai.nodecounter = 1;
+			path = ai.getPath (gameObject.transform.position, attackingg.transform.position);
+			ai.fpscounter = 0;
+		}
+		ai.fpscounter++;
+		if (Vector3.SqrMagnitude(this.transform.position - target) < 0.01) {
+			this.rigidbody2D.velocity = new Vector2(0, 0);
+			if (ai.nodecounter < path.Count - 1)
+				ai.nodecounter++;
+		}
+		target = new Vector3 (path[ai.nodecounter].loc.x, path[ai.nodecounter].loc.y, 0);
+		this.setDirection(target - this.transform.position);
+		Move();
 
 		if (Vector2.Distance (rigidbody2D.transform.position, attacking.transform.position) <= distance && canAttack)
 		{
