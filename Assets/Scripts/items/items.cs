b477f81ff93c2992201dcdc4ec09799e33bf5d89
verbiage <kk13777@yahoo.com>
2014-11-08 19:31:01 -0600

    Added keys and doors
    found under assets/testing.unity
    
    Spacebar to pickup key (potion sprite)
    Spacebar to open door (wall sprite) with key

---
+++ Assets/Scripts/items/items.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class items : MonoBehaviour {
+	public List<int> numKeysByFloor = new List<int>();
+	public int numHealthPotions = 0;
+
+
+
+	void Start ()
+	{
+		for(int i = 0; i < 10; i++)
+		{
+			numKeysByFloor.Add (0);
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+
+
+	}
+
+	public void AddKey(Keys keyToAdd)
+	{
+
+		numKeysByFloor[keyToAdd.floorNumber] = numKeysByFloor[keyToAdd.floorNumber]+1;
+
+	}
+	public bool hasKey(LockedDoor doorToCheck)
+	{
+		if (numKeysByFloor.ToArray()[doorToCheck.floorNum] > 0)
+		{
+			return true;
+		} 
+
+		else
+		{
+			return false;
+		}
+	}
+
+	public void takeKey(LockedDoor doorToDelete)
+	{
+		numKeysByFloor[doorToDelete.floorNum] = numKeysByFloor[doorToDelete.floorNum]-1;
+	}
+
+}
+
