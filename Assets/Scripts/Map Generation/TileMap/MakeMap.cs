d07c5cc2ee149b5943903308ec43a946bfc2d273
Bogdan Enache <enache2@illinois.edu>
2014-11-08 19:58:26 -0600

    Turned caves off, working on generation

--- Assets/Scripts/Map Generation/TileMap/MakeMap.cs
+++ Assets/Scripts/Map Generation/TileMap/MakeMap.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public enum eTile {Unknown, Floor, Wall, Filler, Player, Goal, Enemy};
+public enum eTile {Unknown, dConnectedFloor, dConvertedFiller, Floor, Wall, Filler, Player, Goal, Enemy};
 
 public class MakeMap : MonoBehaviour 
 {
@@ -40,9 +40,9 @@ public class MakeMap : MonoBehaviour
 	{
 		TileMapData map = new TileMapData();
 
-        if(Random.Range(0.0f,2.0f) > 1.0)
-            map.GenCave(xMax,yMax);
-        else
+     //   if(Random.Range(0.0f,2.0f) > 1.0)
+      //      map.GenCave(xMax,yMax);
+      //  else
             map.GenClassic(xMax,yMax, nRooms);
     	 return map;
 	}
