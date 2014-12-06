5af223b8babae64258501555a161d0f92ecb04a3
Wyatt Duiker <duiker2@illinois.edu>
2014-12-06 17:41:12 -0600

    Ghost will spawn every 900 frames(`30s)

--- Assets/Scripts/Map Generation/TileMap/MakeMap.cs
+++ Assets/Scripts/Map Generation/TileMap/MakeMap.cs
@@ -22,6 +22,7 @@ public class MakeMap : MonoBehaviour
 	public int yMax;
 	public int nRooms;
 	public int numEnemies;
+ 	private float enemySpawnTimer;
 
 	private TMDList dungeon = new TMDList(0);
 	private bool toPrevFloor = false;
@@ -38,19 +39,30 @@ public class MakeMap : MonoBehaviour
 	void Start () 
 	{
         DungeonFloor = 0;
+        enemySpawnTimer = 900;
         PlayerInstance = (GameObject) Instantiate(Player, new Vector3(0,0,0), Quaternion.identity);
 		Invoke ("PlaceMap", 0f);
 	}
+
+	private void Update()
+    {
+    	enemySpawnTimer--;
+    	if(enemySpawnTimer<0 && numEnemies<10)
+    	{
+    		Spawning.SpawnEnemies(dungeon.getTMD(DungeonFloor), 1, eGhost, Player);
+    		enemySpawnTimer = 900.0f;
+    	}
+    }
 
     public void EnemySpawningDifficulty(TileMapData map)
     {
 
             if(DungeonFloor < 3)
-                Spawning.SpawnEnemies(map, numEnemies, eRat);
+                Spawning.SpawnEnemies(map, numEnemies, eRat, Player);
             if(DungeonFloor == 4)
-                Spawning.SpawnEnemies(map, 1, eGhost);
+                Spawning.SpawnEnemies(map, 1, eGhost, Player);
             if(DungeonFloor > 4)
-                Spawning.SpawnEnemies(map, numEnemies, eGhost);
+                Spawning.SpawnEnemies(map, numEnemies, eGhost, Player);
 
     }
 
