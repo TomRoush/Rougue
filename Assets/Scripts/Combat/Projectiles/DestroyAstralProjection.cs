a75c5f6d1c6e50770124448fd049e48632cdffc7
John5025 <Moon50255025@yahoo.com>
2014-12-06 16:48:09 -0600

    Balancing, Astral Projection follows player

--- Assets/Scripts/Combat/Projectiles/DestroyAstralProjection.cs
+++ Assets/Scripts/Combat/Projectiles/DestroyAstralProjection.cs
@@ -4,11 +4,21 @@ using System.Collections;
 public class DestroyAstralProjection : DestroyProjectile {
 	
 	private int damage;
+	GameObject player;
 	
 	public void Initialize(int pDamage)
 	{
 		targetTag = "Player";
 		damage = pDamage;
+		player = GameObject.FindGameObjectWithTag(targetTag);
+	}
+
+	private void FixedUpdate () {
+
+		float xval = (player.transform.position.x - gameObject.transform.position.x) ;
+		float yval = (player.transform.position.y - gameObject.transform.position.y) ;
+		Vector3 toward = new Vector3(xval * 25, yval * 25, 1.9f)/player.GetComponent<Status>().getDistance(gameObject);
+		gameObject.GetComponent<Rigidbody2D>().AddForce(toward);
 	}
 	
 	protected override void OnTargetCollision(GameObject contact) {
