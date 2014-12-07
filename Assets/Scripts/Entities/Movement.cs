10dcc8cc2efcf6ae297091587940d482275ad8ac
Tom Roush <birdbrain2@comcast.net>
2014-12-06 23:41:51 -0600

    Enemies face their path target

--- Assets/Scripts/Entities/Movement.cs
+++ Assets/Scripts/Entities/Movement.cs
@@ -12,6 +12,15 @@ public abstract partial class Entities : MonoBehaviour {
         }
 
         protected void Move() {
+			if(direction.x < 0) {
+				Vector3 theScale = transform.localScale;
+				theScale.x = 1;
+				transform.localScale = theScale;
+			} else {
+				Vector3 theScale = transform.localScale;
+				theScale.x = -1;
+				transform.localScale = theScale;
+			}
             this.rigidbody2D.velocity = direction * cStat.getEffectiveSpeed();
         }
 
